c5091866578757d490773d9b6d8f1fae1ac35c1d
Jason Terhorst <yourstruly@jterhorst.com>
2019-04-18 15:06:36 -0500

    Using env locally

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
+require 'dotenv/load'
+
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
--- config/initializers/polleverywhere.rb
+++ config/initializers/polleverywhere.rb
@@ -1,20 +1,10 @@
 
 require "polleverywhere"
 
-if Rails.env.development?
-	poll_username = Rails.application.secrets.poll_username
-	poll_password = Rails.application.secrets.poll_password
-end
-
-if Rails.env.production?
-	poll_username = ENV['poll_username']
-	poll_password = ENV['poll_password']
-end
-
-raise "invalid creds" unless poll_username != nil
+raise "invalid creds" unless ENV['poll_password'] != nil
 
 PollEverywhere.config do
-  username  poll_username
-  password  poll_password
+  username  ENV['poll_username']
+  password  ENV['poll_password']
   url       "https://www.polleverywhere.com"
 end
